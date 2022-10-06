46068bb9d30f90c37b415ea42f94ec51b3998f05
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2022-10-06 16:43:49 +0800

    ввод переменных

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+Console.Clear();
+Console.WriteLine("ВВедите первое число:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("ВВедите второе число:");
+int b = int.Parse(Console.ReadLine()!);
