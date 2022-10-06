d5ba975a125b0b4b3e190db21439852cff138e76
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2022-10-06 16:57:44 +0800

    Решила задачу

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
 //какое число большее, а какое меньшее.
 Console.Clear();
-Console.WriteLine("ВВедите первое число:");
+Console.Write("ВВедите первое число:");
 int a = int.Parse(Console.ReadLine()!);
-Console.WriteLine("ВВедите второе число:");
+Console.Write("ВВедите второе число:");
 int b = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    Console.WriteLine($"Число {a} - большее, а число {b} - меньшее.");
+}
+else
+{
+    Console.WriteLine($"Число {b} - большее, а число {a} - меньшее.");
+}
